8716c74a1940608298cb00795483a4931298a9f2
WebProAditi <167023837+WebProAditi@users.noreply.github.com>
2025-11-23 11:34:49 +0530

    Add files via upload

---
+++ a.js
@@ -0,0 +1,8 @@
+alert("Welcome to Aditi's Portfolio")
+ function changeText() {
+        // document.backgroundColor = "#4458dc";
+        //for contact^^
+
+    innerText = alert("Thanks for clicking! 🚀");
+    innerText = new Date();
+  }
